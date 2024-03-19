c9d4138274ac15699e9a164ef9584ef5f9fee51f
Tristan Alexander Mouritsen <tmouritsen57@gmail.com>
2024-03-18 21:38:41 -0600

    added spinner while loading files

--- src/components/WorkspaceSearch.tsx
+++ src/components/WorkspaceSearch.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Button, Card, Form } from 'react-bootstrap';
+import { Button, Card, Form, Spinner } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 
 import { fetchFilesSuccess } from '../redux/reducers/filesReducer';
@@ -9,10 +9,12 @@ import FileComponent from './FileComponent';
 function WorkspaceSearch() {
     const [workspace, setWorkspace] = useState('')
     const [validWorkspace, setValidWorkspace] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
     const dispatch = useDispatch()
 
     const fetchFiles = async () => {
+        setIsLoading(true)
         try {
             const response = await axios.get(
                 'https://api.optilogic.app/v0/' + workspace + '/files',
@@ -24,8 +26,10 @@ function WorkspaceSearch() {
             );
             dispatch(fetchFilesSuccess(response.data.files));
             setValidWorkspace(true);
+            setIsLoading(false)
         } catch (error) {
             setValidWorkspace(false);
+            setIsLoading(false)
             console.error('Error fetching files:', error);
         }
     };
@@ -38,22 +42,28 @@ function WorkspaceSearch() {
         <div>
             <Card className="pt-3 pb-2 py-5 container">
                 <Card.Body>
-                    <Form className="row align-items-center justify-content-center">
-                        <Form.Group className="mb-3 w-75" controlId="workspace">
-                            <Form.Control 
-                                type="text"
-                                value={workspace}
-                                placeholder="Workspace Name"
-                                onChange={handleChange} 
-                                />
-                        </Form.Group>
-                    </Form>
-                    <Button variant="primary" onClick={fetchFiles}  className="w-25">
-                        Find Files
-                    </Button>
+                    {!isLoading ? (
+                        <>
+                            <Form className="row align-items-center justify-content-center">
+                                <Form.Group className="mb-3 w-75" controlId="workspace">
+                                    <Form.Control 
+                                        type="text"
+                                        value={workspace}
+                                        placeholder="Workspace Name"
+                                        onChange={handleChange} 
+                                        />
+                                </Form.Group>
+                            </Form>
+                            <Button variant="primary" onClick={fetchFiles}  className="w-25">
+                                Find Files
+                            </Button>
+                        </>
+                    ) : (
+                        <Spinner />
+                    )}
                 </Card.Body>
 
-                {validWorkspace ? 
+                {validWorkspace && !isLoading ? 
                     <FileComponent workspace={workspace} /> : 
                     <p>
                         Please submit a valid workspace to query files.
